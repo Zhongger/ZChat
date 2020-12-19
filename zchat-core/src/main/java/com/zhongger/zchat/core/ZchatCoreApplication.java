bec31b6dc46b813a4749f97bb417728bdc9cfd68
Zhongger <973593026@qq.com>
2020-12-19 17:58:14 +0800

    feat:项目初始化

---
+++ zchat-core/src/main/java/com/zhongger/zchat/core/ZchatCoreApplication.java
@@ -0,0 +1,11 @@
+package com.zhongger.zchat.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZchatCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZchatCoreApplication.class,args);
+    }
+}
